44d46b3eb46b11a80547b8c431eb206a02463745
SeiamAlMahmud <samahmud49@gmail.com>
2025-10-11 13:24:38 +0600

    second

---
+++ hello_world/Main.java
@@ -0,0 +1,19 @@
+// class FirstClass {
+//     public static void main(String args[]) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+// package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Tony Stark";
+        int age = 52;
+        float percentage = 99.99f;
+        double total = age + percentage;
+
+        System.out.print(age + " " + name + " " + percentage + " " + total);
+    }
+}
+ 
