a0efe70c4b7182fdd5db1248fca3e685288610f2
SeiamAlMahmud <samahmud49@gmail.com>
2025-10-16 23:54:16 +0600

    javamain and input output

---
+++ hello_world/inout.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class inout {
+
+    public static void main(String[] arg) {
+        // Input 
+        Scanner sc = new Scanner(System.in);
+        // String name = sc.next(); // If I use next then it retun a single word.
+        String name = sc.nextLine(); 
+        // sc.nextInt()
+        // sc.nextFloat()
+        
+        System.out.println(name);
+    }
+}
