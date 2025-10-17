bc45ea4dce727d87ab541694af19ed25803a2150
SeiamAlMahmud <samahmud49@gmail.com>
2025-10-17 15:03:41 +0600

    do while

---
+++ hello_world/loops/dowhile.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class dowhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+               System.out.println(i + ". Hello world");
+               i++;
+        } while (i < 11);
+    }
+}
